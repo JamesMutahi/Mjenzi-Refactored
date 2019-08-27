91f3f25666047525cb2a8d1007d7a40baf0540fc
JamesMutahi <mutahijames0@gmail.com>
2019-08-27 13:58:29 +0300

    Add url for materials

--- mjenzi_api/urls.py
+++ mjenzi_api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import ProjectViewSet
+from .views import ProjectViewSet, MaterialList
 
 router = DefaultRouter()
 router.register('projects', ProjectViewSet, base_name='projects')
 
 urlpatterns = [
-
+    path("projects/<int:pk>/materials/", MaterialList.as_view(), name="materials_list"),
 ]
 
 urlpatterns += router.urls
