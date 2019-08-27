2fa698f28bc490f3b1bd6fb081babe60b61e466c
JamesMutahi <mutahijames0@gmail.com>
2019-08-27 13:50:16 +0300

    Add projects viewset

--- mjenzi_api/views.py
+++ mjenzi_api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import *
+from .serializers import *
+
+from django.contrib.auth import authenticate
+from rest_framework.exceptions import PermissionDenied
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        project = Project.objects.get(pk=self.kwargs["pk"])
+        if not request.user == project.created_by:
+            raise PermissionDenied("You can not delete this project.")
+        return super().destroy(request, *args, **kwargs)
+
