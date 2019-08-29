4325eefa8e624e6f2958bbaadf352a3784e96b10
JamesMutahi <mutahijames0@gmail.com>
2019-08-29 10:28:09 +0300

    reset

--- mjenzi_api/urls.py
+++ mjenzi_api/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
-from .views import ProjectList, ProjectDetail, MaterialList, UserCreate, ReportList, RequestList, LoginView
+from rest_framework.routers import DefaultRouter
+from .views import ProjectViewSet, MaterialList, UserCreate, ReportList, RequestList, LoginView
+
+router = DefaultRouter()
+router.register('projects', ProjectViewSet, base_name='projects')
 
 urlpatterns = [
-    path("projects/", ProjectList.as_view(), name="projects_list"),
-    path("projects/<int:pk>/", ProjectDetail.as_view(), name="projects_detail"),
     path("projects/<int:pk>/reports/", ReportList.as_view(), name="reports_list"),
     path("projects/<int:pk>/materials/", MaterialList.as_view(), name="materials_list"),
     path("projects/<int:pk>/requests/", RequestList.as_view(), name="create_request"),
     path("sign-up/", UserCreate.as_view(), name="user_create"),
     path("login/", LoginView.as_view(), name="login"),
 ]
+
+urlpatterns += router.urls
--- mjenzi_api/views.py
+++ mjenzi_api/views.py
@@ -12,24 +12,7 @@ from rest_framework.exceptions import PermissionDenied
 from django.core.mail import send_mail
 
 
-class ProjectList(generics.ListCreateAPIView):
-    queryset = Project.objects.all()
-    serializer_class = ProjectSerializer
-
-    def post(self, request, *args, **kwargs):
-        a_project = Project.objects.create(
-            project_name=request.data["project_name"],
-            contractor_email=request.data["contractor_email"],
-            description=request.data["description"],
-            user=request.user,
-        )
-        return Response(
-            data=ProjectSerializer(a_project).data,
-            status=status.HTTP_201_CREATED
-        )
-
-
-class ProjectDetail(generics.RetrieveDestroyAPIView):
+class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
 
@@ -51,7 +34,7 @@ class ReportList(generics.ListCreateAPIView):
 
     def post(self, request, *args, **kwargs):
         project = Project.objects.get(pk=self.kwargs["pk"])
-        if not request.user == project.user:
+        if not request.user == project.created_by:
             raise PermissionDenied("You can not create reports for this project.")
         return super().post(request, *args, **kwargs)
 
@@ -62,12 +45,6 @@ class RequestList(generics.ListCreateAPIView):
         return queryset
 
     serializer_class = RequestSerializer
-
-    def post(self, request, *args, **kwargs):
-        project = Project.objects.get(pk=self.kwargs["pk"])
-        if not request.user == project.user:
-            raise PermissionDenied("You can not create requests for this project.")
-        return super().post(request, *args, **kwargs)
 
 
 class UserCreate(generics.CreateAPIView):
