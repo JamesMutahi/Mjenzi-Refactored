4b63c156ced41bef7b938b706fb1c1a605c54b5a
JamesMutahi <mutahijames0@gmail.com>
2019-08-31 13:53:04 +0300

    Remove dates

--- mjenzi_api/models.py
+++ mjenzi_api/models.py
@@ -9,16 +9,16 @@ class Project(models.Model):
     project_name = models.CharField(max_length=100, null=False)
     contractor_email = models.CharField(max_length=100, null=False)
     description = models.TextField(default="no description")
-    date_posted = models.DateTimeField(default=timezone.now)
+    # date_posted = models.DateTimeField(default=timezone.now)
     developer_email = models.CharField(max_length=100, null=False)
-
 
     def __str__(self):
         return self.project_name
 
 
 class Materials(models.Model):
-    MATERIALS = Choices('Cement', 'Brick', 'Sand', 'Ballast', 'Metal rods', 'Roofing tiles', 'Plywood', 'Timber', 'Glass')
+    MATERIALS = Choices('Cement', 'Brick', 'Sand', 'Ballast', 'Metal rods', 'Roofing tiles', 'Plywood', 'Timber',
+                        'Glass')
     material_name = models.CharField(choices=MATERIALS, default=MATERIALS.Cement, max_length=20)
     # quantity
     quantity = models.IntegerField(null=False)
@@ -36,7 +36,7 @@ class Requests(models.Model):
     photo = models.ImageField(default='projects/default.jpeg', upload_to='projects', blank=False)
     location = models.PointField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='projects')
-    date_posted = models.DateTimeField(default=timezone.now)
+    # date_posted = models.DateTimeField(default=timezone.now)
     status = models.CharField(max_length=30, null=True)
 
     def __str__(self):
@@ -49,6 +49,7 @@ class Reports(models.Model):
     photo = models.ImageField(default='projects/default.jpeg', upload_to='projects', null=False)
     location = models.PointField()
     overview = models.TextField(blank=False)
+
     # date_posted = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
