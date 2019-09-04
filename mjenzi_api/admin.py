640bf26a80f48893849d818ab4977da837dde682
zubeir-Abubakar <zubkayare@gmail.com>
2019-09-04 11:02:05 +0300

    importing the models from models.py into the admine file

--- mjenzi_api/admin.py
+++ mjenzi_api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Project, Materials, , Reports
+from .models import Project, Materials, Requests, 
 
 admin.site.register(Project)
 admin.site.register(Materials)
