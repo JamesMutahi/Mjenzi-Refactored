53bcf84ec289d9017c151337148ca34c26ac47e7
zubeir-Abubakar <zubkayare@gmail.com>
2019-09-04 11:04:42 +0300

    adding register site for reports

--- mjenzi_api/admin.py
+++ mjenzi_api/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from .models import Project, Materials, Requests, Reports
 
 admin.site.register(Project)
+admin.site.register(Materials)
 admin.site.register()
-admin.site.register(Reports)
 admin.site.register(Requests)
